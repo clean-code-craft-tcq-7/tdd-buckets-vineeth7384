8c03f6a9269aac114ac27ecf6be7bfe2f2f1ee39
vineeth7384 <67726924+vineeth7384@users.noreply.github.com>
2023-01-26 13:21:30 +0530

    Update test-RangeDetection.cpp

--- test-RangeDetection.cpp
+++ test-RangeDetection.cpp
@@ -26,6 +26,7 @@ TEST_CASE("Current Samples RangeDetection Testcase3") {
 TEST_CASE("Current Samples RangeDetection Testcase4") {
 	int LogCount;
 	ChargingCurrentRangeDetection(3,12,&samples[0],size,Info,&LogCount);
-     REQUIRE(Info[1].Count ==3 );
+        REQUIRE(Info[0].Count ==4 );
+	REQUIRE(Info[1].Count ==3 );
 }
 
