41fcde5538de8b61928729f8b584f4f92ef18056
vineeth7384 <67726924+vineeth7384@users.noreply.github.com>
2023-01-30 03:34:33 +0530

    Update CurrentSamplesRangeDetection.c

--- CurrentSamplesRangeDetection.c
+++ CurrentSamplesRangeDetection.c
@@ -4,10 +4,8 @@
 #include"CurrentSamplesRangeDetection.h"
 
 static char resultBuffer[100]; 
-int StartRange ;
-int EndRange;
-int CountForRangeCalculation = 0;
-int count ;
+CurrentSamplesInfo CurrentSampleInfo[100] ;
+
 
 void ClearResultBuffer()
 {
@@ -27,13 +25,13 @@ void SortArrayInAscendingOrder(int *CurrentSample,int CurrentSamplesize)
     qsort(&CurrentSample[0],CurrentSamplesize,sizeof(int), compareForAscending);
 }
 
-char* PrintCSVFormattedOutput(CurrentSamplesInfo *Report ,int CountForRangeCalculation)
+char* PrintCSVFormattedOutput(CurrentSamplesInfo *Report ,int CountForRangeSeparation)
 {
     
     printf("Range, Readings\n");
     char temp[20];
     ClearResultBuffer();
-    for(int count = 0;count<=CountForRangeCalculation;count++)
+    for(int count = 0;count<=CountForRangeSeparation;count++)
     {
         sprintf(temp,"%d-%d, %d\n",Report[count].Start,Report[count].End,Report[count].Count);
         strcat(resultBuffer,temp);
@@ -41,56 +39,104 @@ char* PrintCSVFormattedOutput(CurrentSamplesInfo *Report ,int CountForRangeCalcu
     }
     return resultBuffer;
 }
-
-void FindRangeofNonDuplicateElements(int *CurrentSample,int i,int end)
+int FindStartIndex(int *CurrentSample , int size, int start )
 {
-    if(CurrentSample[i] +1 == CurrentSample[i+1])
+    int start_index;
+    
+    for( int index = 0; index<size ;index++)
     {
-        count++ ;
+        if( CurrentSample[index]>= start)
+        {
+            start_index = index;
+            break;
+        }
     }
-    else
-    {
-        CountForRangeCalculation +=1 ;
-        count= 1;
-        StartRange = CurrentSample[i+1] ;
-    }
-    EndRange = CurrentSample[i+1];   
+    return start_index;
 }
 
-
-void RangeValidation(int start,int end,int *CurrentSample,int i)
+int FindEndIndex(int *CurrentSample,int size,int start_index,int end)
 {
-    if(CurrentSample[i]>= start && CurrentSample[i+1]<= end)
+    int end_index;
+    
+    for(int index = start_index;index<size;index++)
     {
-        if (CurrentSample[i]==CurrentSample[i+1])
+        if(CurrentSample[index]>end)
         {
-            count = count+2;
+            end_index = index -1;
+            break;
         }
-        else
+        if(index == size - 1)
         {
-            FindRangeofNonDuplicateElements(&CurrentSample[0],i,end);
+           end_index = index ; 
         }
-    } 
+    }
+    return end_index;
+}
+
+int Check_ArrayContinous(int start_index,int end_index,int* CurrentSample,int*count )
+{
+    int FailedArrayIndex = 0;
+    int flag = 1;
+    *count = 0 ;
+    for ( int index = start_index ;index<end_index;index++)
+    {
+        
+        int elementDifference = (CurrentSample[index+1] - CurrentSample[index]) ;
+        (*count)++;
+        if (elementDifference >=2 )
+        {
+            flag = 0;
+            FailedArrayIndex = index + 1;
+            break;
+        }
+    }
+    return flag,FailedArrayIndex ;
+}
+
+void UpdateCurrentSampleInfo( int CountForRangeSeparation,int start_index,int end_index,int count,int*CurrentSample)
+{
+    CurrentSampleInfo[CountForRangeSeparation].Start = CurrentSample[start_index];
+    CurrentSampleInfo[CountForRangeSeparation].End = CurrentSample[end_index];
+    CurrentSampleInfo[CountForRangeSeparation].Count = count;
+    
 }
 
 char* ChargingCurrentRangeDetection(int start,int end,int *CurrentSample,int CurrentSamplesize ) 
 {
-    StartRange = start ;
-    EndRange = end;
-    CountForRangeCalculation = 0;
-    count = 0;
     char*Output ;
-    CurrentSamplesInfo CurrentSampleInfo[CurrentSamplesize] ;
-    SortArrayInAscendingOrder(&CurrentSample[0],CurrentSamplesize);
-    
-    for(int i =0; i< CurrentSamplesize-1; i++)
-    {
-        RangeValidation(start,end,&CurrentSample[0],i);
-        CurrentSampleInfo[CountForRangeCalculation].Start = StartRange;
-        CurrentSampleInfo[CountForRangeCalculation].End = EndRange;
-        CurrentSampleInfo[CountForRangeCalculation].Count = count;
-    }
+    int start_index ,end_index,ContinousFlag,count = 0;
+    int FailedArrayIndex=1;
+    int CountForRangeSeparation = 0;
+    int loopiterationexitCount;
 
-    Output = PrintCSVFormattedOutput (&CurrentSampleInfo[0] ,CountForRangeCalculation);
+    SortArrayInAscendingOrder(&CurrentSample[0],CurrentSamplesize);
+    start_index = FindStartIndex(CurrentSample,CurrentSamplesize,start);
+    end_index = FindEndIndex(CurrentSample,CurrentSamplesize,start_index,end);
+	ContinousFlag, FailedArrayIndex = Check_ArrayContinous(start_index,end_index,CurrentSample,&count) ;
+	if(FailedArrayIndex ==0 )
+	{
+		count = end_index - start_index + 1;
+		UpdateCurrentSampleInfo(CountForRangeSeparation,start_index, end_index, count,CurrentSample);
+	}
+	else 
+	{
+        start_index = 0;
+	    UpdateCurrentSampleInfo(CountForRangeSeparation,start_index, FailedArrayIndex -1, FailedArrayIndex,CurrentSample);
+	    loopiterationexitCount = CurrentSamplesize - FailedArrayIndex;
+
+	    while((FailedArrayIndex != 0) && loopiterationexitCount!=0 )
+	    {
+	        start_index = FailedArrayIndex;
+	        ContinousFlag, FailedArrayIndex = Check_ArrayContinous(start_index,end_index,CurrentSample,&count) ;
+	        CountForRangeSeparation++;
+	        UpdateCurrentSampleInfo(CountForRangeSeparation,start_index, end_index, count,CurrentSample);
+	        loopiterationexitCount--;
+	    }
+	    
+	    
+	}
+	
+
+    Output = PrintCSVFormattedOutput (&CurrentSampleInfo[0],CountForRangeSeparation);
     return Output ;
 }
