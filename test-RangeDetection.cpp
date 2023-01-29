b03aea3fc3696e359b8bd106ecd0c2543ace4c4c
vineeth7384 <67726924+vineeth7384@users.noreply.github.com>
2023-01-29 18:21:25 +0530

    Update test-RangeDetection.cpp

--- test-RangeDetection.cpp
+++ test-RangeDetection.cpp
@@ -45,16 +45,5 @@ TEST_CASE("Given 3,12 as input when ChargingCurrentRangeDetection called then 3-
 	REQUIRE(result == 0 );
 	
 }
-TEST_CASE("Given 3,3 as input when ChargingCurrentRangeDetection called then 3-3,1 is expected ") {
-	int result;
-	char*str = ChargingCurrentRangeDetection(3,3,&samples[0],size);
-	const char *expected = "3-3, 1\n" ;
-	result = strcmp(expected,str);
-	REQUIRE(result == 0 );
-	
-}
-
-
-
 
 
